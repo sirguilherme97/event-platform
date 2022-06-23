aea29f296016302db5ec032413ef0c7203738777
SirGuilhermeBR <sir_guilherme@hotmail.com>
2022-06-22 22:54:23 -0300

    Config Cache Apollo

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,19 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+    },
+}
+
 
 export const client = new ApolloClient({
     uri: 'https://api-sa-east-1.graphcms.com/v2/cl4nkznyp0cd401xm8j974bhh/master',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
 })
