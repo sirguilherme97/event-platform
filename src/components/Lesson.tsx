fc7a5086a364ab4508277a8fa6b37572cb920f30
SirGuilhermeBR <sir_guilherme@hotmail.com>
2022-06-22 21:01:07 -0300

    Aula 03
    Add Vime
    Add React Router Dom

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LesssonProps {
   title: string;
@@ -14,11 +15,11 @@ export function Lesson(props: LesssonProps) {
   const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", { locale: ptBR })
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className='group'>
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -39,6 +40,6 @@ export function Lesson(props: LesssonProps) {
           {props.title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
